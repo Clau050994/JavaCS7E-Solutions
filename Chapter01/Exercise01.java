940e54ef316b6d2fb47e58709a51e71f324ca70a
Claudia <clau050994@gmail.com>
2023-10-25 17:32:00 -0400

    Added solution for Chapter 2, Exercise 1

---
+++ Chapter01/Exercise01.java
@@ -0,0 +1,26 @@
+
+/*1- Name, Age, and Annual Income
+Write a program that declares the following:
+• a String variable named name
+• an int variable named age
+• a double variable named annual Pay
+Store your age, name, and desired annual income as literals in these variables.
+ The program should display these values on the screen in a manner similar to the following:
+My name is Joe Mahoney, my age is 26 and I hope to earn $100000.0 per year.
+2. Name and Initials
+*/
+
+public class Exercise01 {
+
+    public static void main(String[] args){
+    // declaring Variables
+    String name = "Claudia Varona";
+    int age = 25;
+    double annualPay = 150000;
+
+
+    System.out.println("My name is " + name + ", my age is " + age + " and");
+    System.out.println("I hope to earn " + annualPay + " per year");
+
+    }
+}
