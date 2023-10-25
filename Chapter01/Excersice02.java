5be064c669c448fba79b0b1d50376495882f6d32
Claudia <clau050994@gmail.com>
2023-10-25 17:58:41 -0400

    Added solution for Chapter 2, Exercise 2

---
+++ Chapter01/Excersice02.java
@@ -0,0 +1,41 @@
+/*
+* 2. Name and Initials
+Write a program that has the following String variables: firstName, middleMane, and lastName.
+* Initialize these with your first, middle, and last names.
+* The program should al. have the following char variables: firstInitial, middleInitial, and lastInitial.
+Store your first, middle, and last initials in these variables.
+* The program should display the contents of these variables on the screen.
+* */
+
+public class Excersice02 {
+    public static void main(String[] args){
+
+
+        // Creating and initializing Variables
+        String fristName = "Claudia";
+        String middleName = "Cloe";
+        String lastName = "Varona";
+        char firstInitial = 'C';
+        char middleInitial = 'C';
+        char lastInitial = 'V';
+
+        // Printing the values on the screen.
+        System.out.println("My Personal Information:");
+        System.out.println(fristName);
+        System.out.println(middleName);
+        System.out.println(lastName);
+        System.out.println(firstInitial);
+        System.out.println(middleInitial);
+        System.out.println(lastInitial);
+
+
+
+
+
+
+
+
+
+
+    }
+}
